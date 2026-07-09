0ed114caadf3fc2622f7e1e807586bb95835378c
basaranbaran <baranbasaran01@hotmail.com>
2026-07-10 00:28:40 +0300

    chore: configure custom domain dtmehmetyagci.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://basaranbaran.github.io',
-  base: '/dt.mehmet/',
+  site: 'https://dtmehmetyagci.com',
+  base: '/',
   integrations: [tailwind()],
   output: 'static',
 });
